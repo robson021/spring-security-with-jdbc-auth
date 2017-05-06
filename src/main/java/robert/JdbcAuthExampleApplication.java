ca3788c7cca789ba3a9b9e4102c0fa2ef32d894e
robert <nowak021@gmail.com>
2017-05-06 16:56:19 +0200

    initial commit

---
+++ src/main/java/robert/JdbcAuthExampleApplication.java
@@ -0,0 +1,12 @@
+package robert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcAuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcAuthExampleApplication.class, args);
+	}
+}
