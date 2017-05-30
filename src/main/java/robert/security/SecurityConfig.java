8ab6238f3590c284dcfb9dcd634176dfbfa9d812
robert <nowak021@gmail.com>
2017-05-30 23:34:57 +0200

    .

--- src/main/java/robert/security/SecurityConfig.java
+++ src/main/java/robert/security/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,8 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.and().formLogin().permitAll()
 
 				.and().csrf().disable()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
-				.and().headers().cacheControl();
+				.headers()
+				.cacheControl();
 	}
 
 	@Override
